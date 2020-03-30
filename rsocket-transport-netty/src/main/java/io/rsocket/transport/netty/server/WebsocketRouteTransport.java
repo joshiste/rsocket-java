b29343485d4f0db1acdccb0e457aeb9de120e031
Oleh Dokuka <shadowgun@i.ua>
2020-03-30 13:25:33 +0300

    provides bug fix in the introduced implementation
    Signed-off-by: Oleh Dokuka <shadowgun@i.ua>

--- rsocket-transport-netty/src/main/java/io/rsocket/transport/netty/server/WebsocketRouteTransport.java
+++ rsocket-transport-netty/src/main/java/io/rsocket/transport/netty/server/WebsocketRouteTransport.java
@@ -18,11 +18,9 @@ package io.rsocket.transport.netty.server;
 
 import static io.rsocket.frame.FrameLengthFlyweight.FRAME_LENGTH_MASK;
 
-import io.netty.buffer.ByteBufAllocator;
 import io.netty.handler.codec.http.HttpMethod;
 import io.rsocket.Closeable;
 import io.rsocket.DuplexConnection;
-import io.rsocket.fragmentation.FragmentationDuplexConnection;
 import io.rsocket.transport.ServerTransport;
 import io.rsocket.transport.netty.WebsocketDuplexConnection;
 import java.util.ArrayList;
@@ -71,7 +69,7 @@ public final class WebsocketRouteTransport extends BaseWebsocketServerTransport<
   }
 
   @Override
-  public Mono<Closeable> start(ConnectionAcceptor acceptor, int mtu) {
+  public Mono<Closeable> start(ConnectionAcceptor acceptor) {
     Objects.requireNonNull(acceptor, "acceptor must not be null");
 
     return server
@@ -80,7 +78,7 @@ public final class WebsocketRouteTransport extends BaseWebsocketServerTransport<
               routesBuilder.accept(routes);
               routes.ws(
                   hsr -> hsr.method().equals(HttpMethod.GET) && template.matches(hsr.uri()),
-                  newHandler(acceptor, mtu),
+                  newHandler(acceptor),
                   null,
                   FRAME_LENGTH_MASK);
             })
@@ -94,26 +92,14 @@ public final class WebsocketRouteTransport extends BaseWebsocketServerTransport<
     return addRoutes(
         routes,
         hsr -> hsr.method().equals(HttpMethod.GET) && template.matches(hsr.uri()),
-        acceptor,
-        0);
-  }
-
-  public static HttpServerRoutes addRoutes(
-      HttpServerRoutes routes, String path, ConnectionAcceptor acceptor, int mtu) {
-    final UriPathTemplate template = new UriPathTemplate(path);
-    return addRoutes(
-        routes,
-        hsr -> hsr.method().equals(HttpMethod.GET) && template.matches(hsr.uri()),
-        acceptor,
-        mtu);
+        acceptor);
   }
 
   public static HttpServerRoutes addRoutes(
       HttpServerRoutes routes,
       Predicate<? super HttpServerRequest> condition,
-      ConnectionAcceptor acceptor,
-      int mtu) {
-    return routes.ws(condition, newHandler(acceptor, mtu), null, FRAME_LENGTH_MASK);
+      ConnectionAcceptor acceptor) {
+    return routes.ws(condition, newHandler(acceptor), null, FRAME_LENGTH_MASK);
   }
 
   /**
@@ -125,26 +111,8 @@ public final class WebsocketRouteTransport extends BaseWebsocketServerTransport<
    */
   public static BiFunction<WebsocketInbound, WebsocketOutbound, Publisher<Void>> newHandler(
       ConnectionAcceptor acceptor) {
-    return newHandler(acceptor, 0);
-  }
-
-  /**
-   * Creates a new Websocket handler
-   *
-   * @param acceptor the {@link ConnectionAcceptor} to use with the handler
-   * @param mtu the fragment size
-   * @return a new Websocket handler
-   * @throws NullPointerException if {@code acceptor} is {@code null}
-   */
-  public static BiFunction<WebsocketInbound, WebsocketOutbound, Publisher<Void>> newHandler(
-      ConnectionAcceptor acceptor, int mtu) {
     return (in, out) -> {
       DuplexConnection connection = new WebsocketDuplexConnection((Connection) in);
-      if (mtu > 0) {
-        connection =
-            new FragmentationDuplexConnection(
-                connection, ByteBufAllocator.DEFAULT, mtu, false, "server");
-      }
       return acceptor.apply(connection).then(out.neverComplete());
     };
   }
--- rsocket-transport-netty/src/main/java/io/rsocket/transport/netty/server/WebsocketServerTransport.java
+++ rsocket-transport-netty/src/main/java/io/rsocket/transport/netty/server/WebsocketServerTransport.java
@@ -18,9 +18,7 @@ package io.rsocket.transport.netty.server;
 
 import static io.rsocket.frame.FrameLengthFlyweight.FRAME_LENGTH_MASK;
 
-import io.netty.buffer.ByteBufAllocator;
 import io.rsocket.DuplexConnection;
-import io.rsocket.fragmentation.FragmentationDuplexConnection;
 import io.rsocket.transport.ClientTransport;
 import io.rsocket.transport.ServerTransport;
 import io.rsocket.transport.TransportHeaderAware;
@@ -111,31 +109,22 @@ public final class WebsocketServerTransport extends BaseWebsocketServerTransport
   }
 
   @Override
-  public Mono<CloseableChannel> start(ConnectionAcceptor acceptor, int mtu) {
+  public Mono<CloseableChannel> start(ConnectionAcceptor acceptor) {
     Objects.requireNonNull(acceptor, "acceptor must not be null");
 
-    Mono<CloseableChannel> isError = FragmentationDuplexConnection.checkMtu(mtu);
-    return isError != null
-        ? isError
-        : server
-            .handle(
-                (request, response) -> {
-                  transportHeaders.get().forEach(response::addHeader);
-                  return response.sendWebsocket(
-                      null,
-                      FRAME_LENGTH_MASK,
-                      (in, out) -> {
-                        DuplexConnection connection =
-                            new WebsocketDuplexConnection((Connection) in);
-                        if (mtu > 0) {
-                          connection =
-                              new FragmentationDuplexConnection(
-                                  connection, ByteBufAllocator.DEFAULT, mtu, false, "server");
-                        }
-                        return acceptor.apply(connection).then(out.neverComplete());
-                      });
-                })
-            .bind()
-            .map(CloseableChannel::new);
+    return server
+        .handle(
+            (request, response) -> {
+              transportHeaders.get().forEach(response::addHeader);
+              return response.sendWebsocket(
+                  null,
+                  FRAME_LENGTH_MASK,
+                  (in, out) -> {
+                    DuplexConnection connection = new WebsocketDuplexConnection((Connection) in);
+                    return acceptor.apply(connection).then(out.neverComplete());
+                  });
+            })
+        .bind()
+        .map(CloseableChannel::new);
   }
 }
